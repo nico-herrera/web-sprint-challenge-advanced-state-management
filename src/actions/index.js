bac7dc43387fb245e9576f39061b40832998c2a1
Nico Herrera <nherrera0831@gmail,com>
2021-02-06 01:16:11 -0800

    meets mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
+
+export const START_FETCHING = "START_FETCHING";
+export const END_FETCHING = "END_FETCHING";
+export const DATA_RETRIEVED = "DATA_RETRIEVED";
+export const SEND_DATA = "SEND_DATA";
+export const SET_ERROR = "SET_ERROR";
+
+export const fetchSmurfs = () => async (dispatch) => {
+  dispatch({ type: START_FETCHING });
+  try {
+    const data = await axios("http://localhost:3333/smurfs");
+    console.log(data);
+    dispatch({ type: END_FETCHING });
+    dispatch({ type: DATA_RETRIEVED, payload: data });
+  } catch (error) {
+    console.error(error);
+    dispatch({ type: SET_ERROR, payload: error.message });
+  }
+};
+
+export const addSmurf = (data) => async (dispatch) => {
+  try {
+    const sendData = await axios.post("http://localhost:3333/smurfs", data);
+    dispatch({ type: SEND_DATA, payload: sendData });
+  } catch (error) {
+    console.log(error.response);
+    dispatch({ type: SET_ERROR, payload: error.response.data.Error });
+  }
+};
 
 //Task List:
-//1. Add fetch smurfs action: 
+//1. Add fetch smurfs action:
 //              - fetch and return initial list of smurfs
 //              - dispatch actions that indicate if we are waiting for a server response
 //              - dispatch an error text action if an error is returned from the server
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import reducer from "./reducers";
 
 import "./index.css";
 import App from "./App";
 
-const { worker } = require('./mocks/browser');
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const { worker } = require("./mocks/browser");
 worker.start();
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
